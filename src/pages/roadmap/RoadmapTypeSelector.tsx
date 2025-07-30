8d527fae7ee7f6e44e1ad4ad1dfc62084320dccc
redzzzi <myqueen1346@naver.com>
2025-07-30 19:19:20 +0900

    style: 버튼과 출력부 분리

---
+++ src/pages/roadmap/RoadmapTypeSelector.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { capitalizeFirstLetter } from '@src/utils/chars';
+import RoadmapTypeButton from './template/RoadmapTypeButton';
+import { roadmapTypes } from '@src/mocks/data/roadmaps';
+
+const RoadmapTypeSelector = () => {
+  const navigate = useNavigate();
+  const { t } = useTranslation();
+  const [hoveredType, setHoveredType] = useState<string | null>(null);
+
+  const handleRoadmapTypeSelect = (roadmapType: string) => {
+    console.log(`Selected roadmap type: ${roadmapType}`);
+    navigate(`/roadmap/${roadmapType}`);
+  };
+
+  const handleHover = (type: string | null) => {
+    setHoveredType(type);
+  };
+
+  return (
+    <div className="flex flex-col items-center gap-6 py-10">
+      {roadmapTypes.map((category) => {
+        const type = category.type.toLowerCase();
+        const progress = category.progressRatio;
+        const title = capitalizeFirstLetter(type);
+        const description = t(
+          `roadmap.categories.${type.toUpperCase()}.description`
+        );
+
+        return (
+          <RoadmapTypeButton
+            key={category.type}
+            type={type}
+            title={title}
+            description={description}
+            progress={progress}
+            hoveredType={hoveredType}
+            onSelect={handleRoadmapTypeSelect}
+            onHover={handleHover}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default RoadmapTypeSelector;
