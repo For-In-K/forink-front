0adf468187eb84e36ab01027e4420b8cb2dcee4f
redzzzi <myqueen1346@naver.com>
2025-08-02 02:53:54 +0900

    feat: 가이드 페이지 타입 정의

--- src/types/guides.ts
+++ src/types/guides.ts
@@ -1,7 +1,37 @@
-export interface GuideResponse {
+export interface GuideProfile {
   memberId: number;
   name: string;
-  language: string[];
+  language: string;
   nationality: string;
   guideExpCount: number;
+  expertise: string;
 }
+
+export type GetOfficialGuideProfileResponse = GuideProfile[];
+
+export interface GuideFeedback {
+  // now for pre guide
+  feedbackId: number;
+  feedbackTitle: string;
+  feedbackContent: string;
+}
+
+export type GetPreGuideFeedbackResponse = GuideFeedback[];
+
+export interface GuideFeedbackRate {
+  rateId: number;
+  feedbackTitle: string;
+  rateCount: number;
+  averageScore: number;
+  expertiseScore: number;
+  helpScore: number;
+  recommendScore: number;
+}
+
+export type GetPreGuideFeedbackRateResponse = GuideFeedbackRate[];
+
+export interface PreGuideRateStatus {
+  rateStatus: 'IN_PROGRESS' | 'ALMOST';
+}
+
+export type GetPreGuideRateStatusResponse = PreGuideRateStatus;
