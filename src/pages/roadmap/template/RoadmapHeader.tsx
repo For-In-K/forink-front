a2e8424f508f893d2d8025369016cf5c4d2f30e3
redzzzi <myqueen1346@naver.com>
2025-08-01 23:56:42 +0900

    design: 로드맵 헤더 css 수정

--- src/pages/roadmap/template/RoadmapHeader.tsx
+++ src/pages/roadmap/template/RoadmapHeader.tsx
@@ -31,24 +31,29 @@ const RoadmapHeader = ({
 
   return (
     <>
-      <div className="text-text-muted flex flex-col items-start gap-5 md:flex-row md:items-center md:justify-around md:gap-20">
-        <div className="bg-surface text-title2 rounded-full px-6 py-2">
-          {capitalizeFirstLetter(roadmapType)}
-        </div>
-        {headerType === 'detail' && milestoneTitle && (
+      {headerType === 'milestone' && (
+        <div className="text-text-muted flex flex-col items-start gap-5 md:flex-row md:items-center md:justify-around md:gap-20">
           <div className="bg-surface text-title2 rounded-full px-6 py-2">
-            {capitalizeFirstLetter(milestoneTitle)}
+            {capitalizeFirstLetter(roadmapType)}
           </div>
-        )}
-        {headerType === 'milestone' && (
           <div className="flex min-h-10 w-full flex-1 flex-col justify-between md:w-auto">
             <span className="text-body">
               You're {headerRatio}% closer to completing the roadmap!
             </span>
             <Progressbar ratio={headerRatio} />
           </div>
-        )}
-      </div>
+        </div>
+      )}
+      {headerType === 'detail' && milestoneTitle && (
+        <div className="text-text-muted flex gap-5 md:justify-start md:gap-10">
+          <div className="bg-surface text-title2 rounded-full px-6 py-2">
+            {capitalizeFirstLetter(roadmapType)}
+          </div>
+          <div className="bg-surface text-title2 truncate rounded-full px-6 py-2">
+            {capitalizeFirstLetter(milestoneTitle)}
+          </div>
+        </div>
+      )}
     </>
   );
 };
