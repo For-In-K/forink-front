c836e958634269cb603de326d45cdc62b7f9ac19
redzzzi <myqueen1346@naver.com>
2025-08-07 14:08:47 +0900

    design: 헤더 높이에 따라 container mt 속성 추가

--- src/layouts/Container/Container.tsx
+++ src/layouts/Container/Container.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 const Container = () => {
   return (
     <>
-      <div className="container mx-auto w-full px-10 py-25 md:py-30">
+      <div className="container mx-auto mt-[var(--height-min-header)] w-full px-10 py-25 md:mt-[var(--height-header)] md:py-30">
         <Outlet />
       </div>
     </>
