290a5db69859fce47bf44fe4eb050eff06e55acd
redzzzi <myqueen1346@naver.com>
2025-08-08 14:01:03 +0900

    design: mt 속성 추가에 따른 container 패딩 축소

--- src/layouts/Container/Container.tsx
+++ src/layouts/Container/Container.tsx
@@ -8,7 +8,7 @@ const Container = () => {
 
   return (
     <>
-      <div className="container mx-auto mt-[var(--height-min-header)] w-full px-10 py-25 md:mt-[var(--height-header)] md:py-30">
+      <div className="container mx-auto mt-[var(--height-min-header)] w-full px-10 py-10 md:mt-[var(--height-header)] md:py-15">
         <Outlet />
         {isChatbotVisible && <ChatbotWidget />}
       </div>
