c2635ceb0b6dd75a9162d10f29dc04fece3fe4cc
redzzzi <myqueen1346@naver.com>
2025-08-09 19:17:52 +0900

    feat: 토스트 적용

--- src/pages/auth/SigninCallback.tsx
+++ src/pages/auth/SigninCallback.tsx
@@ -1,27 +1,38 @@
 import { useEffect } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
+
+import { toast } from 'react-toastify';
+import useAuth from '@hooks/useAuth';
 import { getOauthCallback } from '@apis/oauth';
 
 const SigninCallback = () => {
-  const [params] = useSearchParams();
   const navigate = useNavigate();
+  const { signIn } = useAuth();
+
+  const [params] = useSearchParams();
+  const code = params.get('code');
 
   useEffect(() => {
-    const code = params.get('code');
-    if (code) {
-      getOauthCallback(code)
-        .then(() => {
-          navigate('/');
-        })
-        .catch(() => {
-          alert('로그인에 실패했습니다. 다시 시도해주세요.'); // TODO: 토스트로 대체하기
-          navigate('/signin');
-        });
-    } else {
-      alert('로그인 코드가 없습니다.');
+    if (!code) {
+      toast('로그인에 실패했어요');
       navigate('/signin');
+      return;
     }
-  }, [params, navigate]);
+
+    const handleSignin = async () => {
+      try {
+        const { token } = await getOauthCallback(code);
+        signIn(token);
+        toast('로그인에 성공했어요');
+        navigate('/');
+      } catch {
+        toast('로그인에 실패했어요');
+        navigate('/signin');
+      }
+    };
+
+    handleSignin();
+  }, [code, navigate, signIn]);
 
   return <div>로그인 처리 중 ...</div>;
 };
