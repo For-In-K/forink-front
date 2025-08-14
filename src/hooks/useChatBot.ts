8629f738a72c1f4b900b053c9ab50aa5fd870f16
redzzzi <myqueen1346@naver.com>
2025-08-15 07:39:35 +0900

    refactor: 챗봇 커스텀 훅 정의

--- src/hooks/useChatBot.ts
+++ src/hooks/useChatBot.ts
@@ -5,26 +5,29 @@ import { createChatBot, exchangeChatMessage } from '@apis/chat';
 export const useCreateChatBot = () => {
   return useMutation({
     mutationFn: createChatBot,
-    onSuccess: () => toast.success('챗봇 Fori가 생성되었어요!'),
-    onError: () => toast.error('챗봇 Fori 생성에 실패했어요'),
+    // onSuccess: () => toast.success('챗봇 Fori가 생성되었어요!'),
+    // onError: () => toast.error('챗봇 Fori 생성에 실패했어요'),
   });
 };
 
 export const useExchangeChatMessage = () => {
   return useMutation({
     mutationFn: exchangeChatMessage,
-    onSuccess: () => toast.success('메시지가 성공적으로 교환되었어요!'),
-    onError: () => toast.error('메시지 교환에 실패했어요'),
+    // onSuccess: () => toast.success('메시지가 성공적으로 교환되었어요!'),
+    // onError: () => toast.error('메시지 교환에 실패했어요'),
   });
 };
 
 const useChatBot = () => {
-  const { mutate: createChatBot } = useCreateChatBot();
-  const { mutate: exchangeChatMessage } = useExchangeChatMessage();
+  const { mutate: createChatBot, isPending: isCreating } = useCreateChatBot();
+  const { mutate: exchangeChatMessage, isPending: isExchanging } =
+    useExchangeChatMessage();
 
   return {
     createChatBot,
+    isCreating,
     exchangeChatMessage,
+    isExchanging,
   };
 };
 
