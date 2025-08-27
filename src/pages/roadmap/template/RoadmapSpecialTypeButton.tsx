7701427a993953149b5e8ead1fc005c48ef7b09b
redzzzi <myqueen1346@naver.com>
2025-08-28 04:01:32 +0900

    design: 로드맵 버튼 css 최종

--- src/pages/roadmap/template/RoadmapSpecialTypeButton.tsx
+++ src/pages/roadmap/template/RoadmapSpecialTypeButton.tsx
@@ -9,6 +9,35 @@ interface RoadmapSpecialTypeButtonProps {
   onHover: (type: string | null) => void;
 }
 
+const colorClasses: Record<
+  string,
+  {
+    hover: string;
+    normal: string;
+    imgHover: string;
+    imgNormal: string;
+  }
+> = {
+  TRAVEL: {
+    hover: 'bg-amber-400 ',
+    normal: 'bg-amber-500',
+    imgHover: 'from-amber-400',
+    imgNormal: 'from-amber-500',
+  },
+  LANGUAGE: {
+    hover: 'bg-emerald-400 ',
+    normal: 'bg-emerald-500',
+    imgHover: 'from-emerald-400',
+    imgNormal: 'from-emerald-500',
+  },
+  CAREER: {
+    hover: 'bg-purple-400 ',
+    normal: 'bg-purple-500',
+    imgHover: 'from-purple-400',
+    imgNormal: 'from-purple-500',
+  },
+};
+
 const RoadmapSpecialTypeButton = ({
   roadmapTypeButtonInfo,
   hoveredType,
@@ -24,7 +53,9 @@ const RoadmapSpecialTypeButton = ({
       onMouseEnter={() => onHover(roadmapTypeButtonInfo.type)}
       onMouseLeave={() => onHover(null)}
       className={`flex min-h-32 w-full max-w-2xl min-w-0 overflow-hidden rounded-3xl shadow-md transition-all duration-300 hover:scale-105 ${
-        isHovered ? 'bg-blue-300 blur-xs filter' : 'bg-blue-400'
+        isHovered
+          ? colorClasses[roadmapTypeButtonInfo.type].hover
+          : colorClasses[roadmapTypeButtonInfo.type].normal
       } md:h-42`}
     >
       <div className="flex flex-2 flex-col items-start gap-1 px-6 py-4 md:px-10 md:py-6">
@@ -42,7 +73,7 @@ const RoadmapSpecialTypeButton = ({
           className="h-full w-full object-cover"
         />
         <div
-          className={`absolute inset-0 bg-gradient-to-r ${isHovered ? 'from-blue-300' : 'from-blue-400'} to-white/10`}
+          className={`absolute inset-0 bg-gradient-to-r ${isHovered ? colorClasses[roadmapTypeButtonInfo.type].imgHover : colorClasses[roadmapTypeButtonInfo.type].imgNormal} to-white/10`}
         />
       </div>
     </button>
