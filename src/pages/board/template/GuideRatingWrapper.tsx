8e9f0ee24cee3bb8903ab09a53fae1e7470bb7d6
redzzzi <myqueen1346@naver.com>
2025-08-29 00:59:19 +0900

    feat: 가이드 페이지 시연

--- src/pages/board/template/GuideRatingWrapper.tsx
+++ src/pages/board/template/GuideRatingWrapper.tsx
@@ -2,30 +2,35 @@ import RatingUnit from './RatingUnit';
 import { usePreGuideFeedbacks } from '@hooks/useGuides';
 import RatingUnitSkeleton from './RatingUnitSkeleton';
 
-const RatingWrapper = () => {
-  const {
-    data: preGuideFeedbacks = [],
-    isLoading,
-    isError,
-  } = usePreGuideFeedbacks();
+import { preGuideFeedbacks } from '@mocks/data/guides';
 
-  if (isError) {
-    return (
-      <div className="rounded-md border border-red-200 bg-red-50 p-4 text-red-700">
-        가이드 평가를 불러오는 데 실패했어요. 나중에 다시 시도해주세요.
-      </div>
-    );
-  }
+const RatingWrapper = () => {
+  // const {
+  //   data: preGuideFeedbacks = [],
+  //   isLoading,
+  //   isError,
+  // } = usePreGuideFeedbacks();
+
+  // if (isError) {
+  //   return (
+  //     <div className="rounded-md border border-red-200 bg-red-50 p-4 text-red-700">
+  //       가이드 평가를 불러오는 데 실패했어요. 나중에 다시 시도해주세요.
+  //     </div>
+  //   );
+  // }
 
   return (
     <div className="flex flex-col gap-10">
-      {isLoading
+      {/* {isLoading
         ? Array.from({ length: 6 }).map((_, index) => (
             <RatingUnitSkeleton key={index} />
           ))
         : preGuideFeedbacks.map((feedback) => (
             <RatingUnit key={feedback.feedbackId} feedback={feedback} />
-          ))}
+          ))} */}
+      {preGuideFeedbacks.map((feedback) => (
+        <RatingUnit key={feedback.feedbackId} feedback={feedback} />
+      ))}
     </div>
   );
 };
